f4b6dc6a89b9a21507fa3db011e0e6cb5a11047b
SexyBoy12 <russianthief3@gmail.com>
2024-05-26 18:12:06 +0300

    change Healthy controller fix error

--- controllers/health_check_controller.py
+++ controllers/health_check_controller.py
@@ -5,13 +5,4 @@ health_bp = Blueprint('health_bp', __name__)
 
 @health_bp.route('/health', methods=['GET'])
 def health_check():
-    service_name = "flaskapp.service"  # Замените на имя вашего сервиса
-    try:
-        result = subprocess.run(["systemctl", "status", service_name], stdout=subprocess.PIPE, text=True)
-        status = result.stdout.strip()
-        if status == "active":
-            return jsonify({'status': 'Running'})
-        else:
-            return jsonify({'status': 'Not Running'})
-    except Exception as e:
-        return jsonify({'status': 'Error', 'message': str(e)}), 500
+    return jsonify({'status': 'Running'})

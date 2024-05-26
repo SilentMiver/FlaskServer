a0e7b4fc904ed77592bf57be478163c083ea7110
SexyBoy12 <russianthief3@gmail.com>
2024-05-26 16:53:08 +0300

    change Healthy controller fix error

--- controllers/health_check_controller.py
+++ controllers/health_check_controller.py
@@ -5,7 +5,7 @@ health_bp = Blueprint('health_bp', __name__)
 
 @health_bp.route('/health', methods=['GET'])
 def health_check():
-    service_name = "your-service-name"  # Замените на имя вашего сервиса
+    service_name = "flaskapp.service"  # Замените на имя вашего сервиса
     try:
         result = subprocess.run(["systemctl", "status", service_name], stdout=subprocess.PIPE, text=True)
         status = result.stdout.strip()
